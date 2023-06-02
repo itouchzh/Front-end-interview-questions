9e7c82f455f72a8e3783ef81b5fb56e654f2d5b6
itouchzh <tootatoo666@gmail.com>
2023-06-02 21:29:13 +0800

    interview

--- frontend/typescript/base/interview.ts
+++ frontend/typescript/base/interview.ts
@@ -121,3 +121,175 @@ const configs = [
 
 type ConfigName = typeof configs[number]['name'] | typeof configs[number]['value']; //type ConfigName = "width" | "60px" | "height" | "40px"
 
+
+//实现convert函数，将数组转换为树结构
+
+const nodes = [
+
+    { id: 3, name: '节点C', parentId: 1 },
+  
+    { id: 6, name: '节点F', parentId: 3 },
+  
+    { id: 0, name: 'root', parentId: null },
+  
+    { id: 1, name: '节点A', parentId: 0 },
+  
+    { id: 8, name: '节点H', parentId: 4 },
+  
+    { id: 4, name: '节点D', parentId: 1 },
+  
+    { id: 2, name: '节点B', parentId: 0 },
+  
+    { id: 5, name: '节点E', parentId: 2 },
+  
+    { id: 7, name: '节点G', parentId: 2 },
+  
+    { id: 9, name: '节点I', parentId: 5 }
+  
+  ]
+  
+  function convert(nodes){
+  
+      let root = null;
+  
+      // 编写转换逻辑
+  
+      return root;
+  
+  }
+  
+  const newTree = convert(nodes)
+  
+  console.log(JSON.stringify(newTree));
+  
+  /**
+  
+  //期望输出
+  
+  {
+  
+      "id":0,
+  
+      "name":"root",
+  
+      "parentId":null,
+  
+      "children":[
+  
+          {
+  
+              "id":1,
+  
+              "name":"节点A",
+  
+              "parentId":0,
+  
+              "children":[
+  
+                  {
+  
+                      "id":3,
+  
+                      "name":"节点C",
+  
+                      "parentId":1,
+  
+                      "children":[
+  
+                          {
+  
+                              "id":6,
+  
+                              "name":"节点F",
+  
+                              "parentId":3
+  
+                          }
+  
+                      ]
+  
+                  },
+  
+                  {
+  
+                      "id":4,
+  
+                      "name":"节点D",
+  
+                      "parentId":1,
+  
+                      "children":[
+  
+                          {
+  
+                              "id":8,
+  
+                              "name":"节点H",
+  
+                              "parentId":4
+  
+                          }
+  
+                      ]
+  
+                  }
+  
+              ]
+  
+          },
+  
+          {
+  
+              "id":2,
+  
+              "name":"节点B",
+  
+              "parentId":0,
+  
+              "children":[
+  
+                  {
+  
+                      "id":5,
+  
+                      "name":"节点E",
+  
+                      "parentId":2,
+  
+                      "children":[
+  
+                          {
+  
+                              "id":9,
+  
+                              "name":"节点I",
+  
+                              "parentId":5
+  
+                          }
+  
+                      ]
+  
+                  },
+  
+                  {
+  
+                      "id":7,
+  
+                      "name":"节点G",
+  
+                      "parentId":2
+  
+                  }
+  
+              ]
+  
+          }
+  
+      ]
+  
+  }
+  
+  */
+
+  
