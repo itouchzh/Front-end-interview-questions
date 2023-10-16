be60f668121e47e69fb9baed3b4bd0c2a0725bc4
itouchzh <tootatoo666@gmail.com>
2023-10-16 08:43:34 +0800

    update js

--- frontend/books/guide/class4/index.js
+++ frontend/books/guide/class4/index.js
@@ -2,25 +2,25 @@
 // number string null undefined boolean bigInt Symbol
 // 引用数据类型 object array function
 
-const a = [1, 2, 3]
-delete a[2]
-console.log(2 in a)
-console.log(a)
+// const a = [1, 2, 3]
+// delete a[2]
+// console.log(2 in a)
+// console.log(a)
 
-const obj = {
-    name: 'paul',
-    age: 23,
-}
+// const obj = {
+//     name: 'paul',
+//     age: 23,
+// }
 
-for (const [key, value] of Object.entries(obj)) {
-    console.log(key, value)
-}
+// for (const [key, value] of Object.entries(obj)) {
+//     console.log(key, value)
+// }
 
-const map = new Map()
-map.set('name', 'paul')
-for (const [key, value] of map) {
-    console.log(key, value)
-}
+// const map = new Map()
+// map.set('name', 'paul')
+// for (const [key, value] of map) {
+//     console.log(key, value)
+// }
 
 // console.log(fn);
 // if (true) {
@@ -29,23 +29,42 @@ for (const [key, value] of map) {
 // }
 // console.log(fn);
 
-f = function () {
-    return true
+const obj = {
+    name: 'paul',
+    age: 23,
+    getName: function () {
+        console.log('my name is paul')
+    },
 }
-g = function () {
-    return false
-}
-    
-    
-(function () {
-    if (g() && [] == ![]) {
-        f = function () {
-            return false
-        }
-        function g() {
-            return true
-        }
-    }
-})()
-console.log(f())
-console.log(g())
+
+// console.log('name' in obj) // true
+// console.log('toString' in obj) // true
+
+// console.log(obj.hasOwnProperty('name')) // true
+// console.log(obj.hasOwnProperty('toString')) // false
+
+// const obj1 = {}
+// Object.assign(obj1, obj)
+// console.log(obj1);
+// obj1.getName()
+
+// const date = new Date()
+// console.log(date);
+// console.log(typeof date);
+// const jDate = JSON.stringify(date)
+// const pDate = JSON.parse(jDate)
+// console.log(jDate, pDate);
+// console.log(typeof pDate);
+
+// const arr1 = [1, 2, 3, 4]
+// const arr2 = arr1
+// arr2[1] = 100
+// console.log(arr1)
+// let a = new Array(4, 5, 6, 6, 7)
+// console.log(a)
+let a = [1, 2, 3, 4, 5]
+let b = Array.from(a, (value, key) => {
+    console.log(value, key)
+    return key + value
+})
+console.log(b);
